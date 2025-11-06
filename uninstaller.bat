6a59debdbd540e4f7bac33687ea57d046711d2b6
ArseniyGitHub <110282586+ArseniyGitHub@users.noreply.github.com>
2025-11-06 21:55:55 +0300

    Add files via upload
    fix (delete from autostart folder and regedit)

--- uninstaller.bat
+++ uninstaller.bat
@@ -3,3 +3,12 @@ timeout /t 8
 taskkill /IM max.exe /f
 taskkill /IM max-service.exe /f
 Remove-Item 'C:\.MAX' -Recurse -Force -ErrorAction SilentlyContinue
+
+set "filename=abob.lnk"
+set "regkey=abob"
+
+:: Удаление файла из папки автозагрузки текущего пользователя
+del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\%filename%" 2>nul
+
+:: Удаление из реестра (HKCU - текущий пользователь)
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "%regkey%" /f 2>nul
